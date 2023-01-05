ef13713ff662d4b8a1c446074cfd4efce828fd7e
Gabe <gabriel.lupino@vtsd.com>
2023-01-05 09:03:24 -0500

    kljsbnlujb

--- Lab11avst.java
+++ Lab11avst.java
@@ -21,18 +21,31 @@ public class Lab11avst
 
     public static void computePrimes(boolean primes[])
     {
-        System.out.println("\nCOMPUTING PRIME NUMBERS");
-
+        for(int k=2;k< primes.length;k++){
+            primes[k] = true;
     }
+        // Step 2: Removing multiples of primes
+        //hint: nested loop
+        //use different variable for outside loop that adds 1 to itself every loop
+
+        for(int index = 2; index < primes.length; index++){
+            for(int k = index * 2; k < primes.length; k+=index){
+                primes[k] = false;
+            }
+            }
+        System.out.println("\nCOMPUTING PRIME NUMBERS");
+        }
 
     public static void displayPrimes(boolean primes[])
     {
         System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
         System.out.println();
 
+        for(int k=1;k< primes.length;k++){
+            if(primes[k]== true)
+            System.out.print(k + " ");
+        }
     }
 
 }
-
-
 
